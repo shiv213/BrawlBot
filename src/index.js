feb0aa6b045ee12c27c9096e307413a6da761878
Ahaan Kanaujia <kanaujia.ahaan@gmail.com>
2022-03-26 04:22:58 -0500

    server file

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ client.once("ready", () => {
         .filter(file => file.endsWith(".js")); // Get and filter all the files in the "Commands" Folder.
 
     // Loop through the command files
+    console.log(commandFiles);
     for (const file of commandFiles) {
         const command = require(`./commands/${file}`); // Get and define the command file.
         commands.set(command.data.name, command); // Set the command name and file for handler to use.
